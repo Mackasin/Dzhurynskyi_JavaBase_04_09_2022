91b0203bef8d8bf784a71cfe6439435e9eaad34e
Dzhurynskyi Maksym <maxsdzh52@gmail.com>
2022-09-05 19:44:16 +0300

    My homeworks

---
+++ src/main/java/com/hillel/dzhurynskyi/homework/hm2/MyApp.java
@@ -0,0 +1,11 @@
+package com.hillel.dzhurynskyi.homework.hm2;
+
+public class MyApp {
+    public static void main(String[] args) {
+        double lat = 26.9029;
+        double lon = 88.4733;
+        char ch = '\u00B0';
+        System.out.println("Coronation bridge:");
+        System.out.println("Latitude: " + lon + ch + "   " + "Latitude: " + lat + ch);
+    }
+}
