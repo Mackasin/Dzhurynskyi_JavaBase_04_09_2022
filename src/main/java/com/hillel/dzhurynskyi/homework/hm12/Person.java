61bf34847d77dcb6bb28f0fd4025f84b9811b7f3
Dzhurynskyi Maksym <maxsdzh52@gmail.com>
2022-10-02 11:06:38 +0300

    Homework 12

--- src/main/java/com/hillel/dzhurynskyi/homework/hm12/Person.java
+++ src/main/java/com/hillel/dzhurynskyi/homework/hm12/Person.java
@@ -3,7 +3,9 @@ package com.hillel.dzhurynskyi.homework.hm12;
 public class Person {
     public static void main(String[] args) {
         System.out.println(personInfo("Will","Smith","New York","2936729462846"));
+
         System.out.println(personInfo("Jackie","Chan","Shanghai","12312412412"));
+
         System.out.println(personInfo("Sherlock","Holmes","London","37742123513"));
     }
     public static String personInfo(String name, String surname, String city, String phoneNumber){
