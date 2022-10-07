096d3982b6d712c7fe560d499686a207b8e3226f
Dzhurynskyi Maksym <maxsdzh52@gmail.com>
2022-10-07 19:48:34 +0300

    Homework 14

--- src/main/java/com/hillel/dzhurynskyi/homework/hm14/FitnessTracker.java
+++ src/main/java/com/hillel/dzhurynskyi/homework/hm14/FitnessTracker.java
@@ -2,18 +2,38 @@ package com.hillel.dzhurynskyi.homework.hm14;
 
 public class FitnessTracker {
     final String name;
-    String surname;
     final int day;
     final String month;
     final int year;
     final String email;
+    final String phoneNumber;
+    String surname;
+    int age;
+    int weight;
+    int pressure;
+    int stepsPerDay;
+
+    public FitnessTracker(String name, String surname, int day, String month, int year, String email, String phoneNumber, int weight, int pressure, int stepsPerDay) {
+        this.name = name;
+        this.surname = surname;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.weight = weight;
+        this.pressure = pressure;
+        this.stepsPerDay = stepsPerDay;
+    }
+
+    public static void printAccountInfo(FitnessTracker fitnessTracker) {
+        System.out.println("Name: " + fitnessTracker.getName() + "\n" + "Surname: " + fitnessTracker.getSurname() + "\n" + "Birthday: " + fitnessTracker.getDay() + "." + fitnessTracker.getMonth() + "." + fitnessTracker.getYear() + "\n" + "Email: " + fitnessTracker.getEmail() + "\n" + "Phone: " + fitnessTracker.getPhoneNumber() + "\n" + "Weight: " + fitnessTracker.getWeight() + "\n" + "Pressure: " + fitnessTracker.getPressure() + "\n" + "Steps per day: " + fitnessTracker.getStepsPerDay() + "\n" + "Age: " + fitnessTracker.getAge() + "\n");
+    }
 
     public int getAge() {
         age = 2020 - year;
         return age;
     }
-
-    int age;
 
     public String getName() {
         return name;
@@ -69,35 +89,5 @@ public class FitnessTracker {
 
     public void setStepsPerDay(int stepsPerDay) {
         this.stepsPerDay = stepsPerDay;
-    }
-
-    final String phoneNumber;
-    int weight;
-    int pressure;
-    int stepsPerDay;
-
-    public FitnessTracker(String name, String surname, int day, String month, int year, String email, String phoneNumber, int weight, int pressure, int stepsPerDay) {
-        this.name = name;
-        this.surname = surname;
-        this.day = day;
-        this.month = month;
-        this.year = year;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.weight = weight;
-        this.pressure = pressure;
-        this.stepsPerDay = stepsPerDay;
-    }
-
-    public static void printAccountInfo(FitnessTracker fitnessTracker) {
-        System.out.println("Name: " + fitnessTracker.getName() + "\n" +
-                "Surname: " + fitnessTracker.getSurname() + "\n" +
-                "Birthday: " + fitnessTracker.getDay() + "." + fitnessTracker.getMonth() + "." + fitnessTracker.getYear() + "\n" +
-                "Email: " + fitnessTracker.getEmail() + "\n" +
-                "Phone: " + fitnessTracker.getPhoneNumber() + "\n" +
-                "Weight: " + fitnessTracker.getWeight() + "\n" +
-                "Pressure: " + fitnessTracker.getPressure() + "\n" +
-                "Steps per day: " + fitnessTracker.getStepsPerDay() + "\n" +
-                "Age: " + fitnessTracker.getAge() + "\n");
     }
 }
