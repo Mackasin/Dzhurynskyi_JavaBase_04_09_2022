5e70e60e1270fc408cdf85c93d77ad7ace7d7df8
Dzhurynskyi Maksym <maxsdzh52@gmail.com>
2022-10-18 13:41:22 +0300

    Homework 17

--- src/main/java/com/hillel/dzhurynskyi/homework/hm17/Drinks.java
+++ src/main/java/com/hillel/dzhurynskyi/homework/hm17/Drinks.java
@@ -1,9 +1,17 @@
 package com.hillel.dzhurynskyi.homework.hm17;
 
 public class Drinks {
+    private static final int COFFEE = 15;
+    private static final int TEE = 10;
+    private static final int LEMONADE = 20;
+    private static final int MOJITO = 25;
+    private static final int MINERAL_WATER = 5;
+    private static final int COCA_COLA = 12;
+
     public static int getCOFFEE() {
         return COFFEE;
     }
+
     public static int getTEE() {
         return TEE;
     }
@@ -23,12 +31,5 @@ public class Drinks {
     public static int getCocaCola() {
         return COCA_COLA;
     }
-
-    private static final int COFFEE = 15;
-    private static final int TEE = 10;
-    private static final int LEMONADE = 20;
-    private static final int MOJITO = 25;
-    private static final int MINERAL_WATER = 5;
-    private static final int COCA_COLA = 12;
 
 }
